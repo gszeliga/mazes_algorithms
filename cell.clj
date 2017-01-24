d388558574989224174b51e3f9446185051b7e4f
Guillermo Szeliga <gszeliga@gmail.com>
2017-01-24 23:51:02 +0100

    Complete initial definition of a Cell

--- cell.clj
+++ cell.clj
@@ -1,14 +1,33 @@
 (defrecord Cell [row column links])
 
 (defprotocol Linkable
-  (linked? [cell])
-  (link [cell, bidi])
-  (unlink [cell, bidi])
-  (links [cell]))
+  (linked? [self cell])
+  (link [self cell bidi])
+  (unlink [self cell bidi])
+  (links [self]))
+  ;This one involves north, south, east & west
+  ;(neighbors [self]))
+
+(extend-protocol Linkable
+  Cell
+  (linked? [self cell]
+           (contains? (-> self :links deref) cell))
+
+  (link [self cell bidi]
+    (dosync
+      (alter (:links self) assoc cell true))
+    (when bidi (link cell self false)))
+
+  (unlink [self cell bidi]
+    (dosync
+      (alter (:links self) dissoc cell))
+    (when bidi (unlink cell self false)))
+
+  (links [self] (keys (-> self :links deref))))
 
 (defn make-cell
   [& {:keys [row column links] :or {links {}}}]
-  (Cell. row column links))
+  (Cell. row column (ref links)))
 
 ; (defn make-cell
 ;    ([row column] (make-cell row column {}))
