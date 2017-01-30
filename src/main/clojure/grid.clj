ab3b3d0d32048b1ebd93a47a437c232a73620283
Guillermo Szeliga <gszeliga@gmail.com>
2017-01-30 23:50:39 +0100

    Begin definition of complementary Grid functions

--- src/main/clojure/grid.clj
+++ src/main/clojure/grid.clj
@@ -3,13 +3,29 @@
 (require 'cell)
 
 (defn make-grid [rows columns]
-  (into [] (map
-            (fn [r]
-              (into [] (map
-                        (fn [c] (cell/make-cell :row r :column c))
-                        (range columns))))
-            (range rows))))
+  (with-meta (into [] (map
+                       (fn [r]
+                         (into [] (map
+                                   (fn [c] (cell/make-cell :row r :column c))
+                                   (range columns))))
+                       (range rows)))
+    {:rows rows :columns columns}))
 
-(defn neighbors-of [grid cell])
+(defn cell-at [grid row col]
+  (when (and (<= 0 row (-> grid meta :rows))
+             (<= 0 col (-> grid meta :columns)))
+    (get-in grid [row col])))
+
+(defn neighbors-from [grid cell]
+  (let [row (:row cell)
+        col (:column cell)]
+    (map #(apply cell-at grid %) (neighbors-at row col))))
+
+(defn- neighbors-at [row column]
+  [[(- 1 row) column]
+   [(+ 1 row) column]
+   [row (- 1 column)]
+   [row (+ 1 column)]])
+
 (defn rows-from [grid])
 (defn cells-from [grid])
