3c295c96d3374d7d9cc49152813c33a2251d5b96
Guillermo Szeliga <gszeliga@gmail.com>
2017-02-14 23:52:14 +0100

    A rough non-tested version of 'sideways' algorithm done

--- src/main/clojure/mazes/algorithms/sidewinder.clj
+++ src/main/clojure/mazes/algorithms/sidewinder.clj
@@ -1,8 +1,29 @@
 (ns mazes.algorithms.sidewinder
-  (use [mazes.grid :only (rows-from cells-from neightbors-from)])
-  (require [mazes.cell :refer:all]))
+  (use [mazes.grid :only (rows-from cells-from neighbors-from)])
+  (require [mazes.cell :refer :all]))
 
 (defn visit [grid]
   (doseq [row (rows-from grid)]
-    ;recursion sounds like a better fit since we need to short-circuit when row's closed out
-    (reduce (fn [visited cell]) [] row)))
+
+    (defn traverse-cells [tmp remaining]
+      (let [cell (first remaining)
+            neighbors (neighbors-from cell grid)
+            visited (conj tmp cell)]
+
+        (if (or (-> neighbors :east nil?)         ;did we reach the most eastern cell?
+                (and (-> neighbors :north some?)  ;can we still move up?
+                     (= 0 (rand-int 2))))
+          (do
+            (when (-> neighbors :north some?)
+              (let [member (rand-nth visited)]
+                (when-let [northern-member (:north (neighbors-from member grid))]
+                  (link member northern-member))))
+            (recur [] (rest remaining)))
+
+          (do
+            (link cell (:east neighbors))
+            (recur visited (rest remaining))))))
+
+    (traverse-cells [] (cells-from row)))
+
+  grid)
