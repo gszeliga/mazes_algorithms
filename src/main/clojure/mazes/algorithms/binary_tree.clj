a45545644cd62018d5e9436fdbe89c4815804693
Guillermo Szeliga <gszeliga@gmail.com>
2017-03-29 00:08:56 +0200

    Emit events from binary-tree algorithm

--- src/main/clojure/mazes/algorithms/binary_tree.clj
+++ src/main/clojure/mazes/algorithms/binary_tree.clj
@@ -1,12 +1,27 @@
 (ns mazes.algorithms.binary-tree
   (use [mazes.grid :only (cells-from neighbors-from)])
+  (use [mazes.algorithms.events :only (wall-down-emiter visiting-cell-emiter)])
   (require [mazes.cell :refer :all]))
 
-(defn visit [grid]
-  (doseq [cell (cells-from grid)]
-    (let [neighbors (neighbors-from cell grid)
-          candidates (filter some? [(:north neighbors)
-                                    (:east neighbors)])]
-      (when-not (empty? candidates)
-        (link cell (rand-nth candidates)))))
-  grid)
+(defn visit
+  ([grid] (visit grid (fn [_])))
+  ([grid f]
+
+   (def wall-down
+     (wall-down-emiter f))
+
+   (def visiting
+     (visiting-cell-emiter f))
+
+   (doseq [cell (cells-from grid)]
+
+     (visiting cell)
+
+     (let [neighbors (neighbors-from cell grid)
+           candidates (filter some? [(:north neighbors)
+                                     (:east neighbors)])]
+       (when-not (empty? candidates)
+         (let [rand-cell (rand-nth candidates)]
+           (wall-down cell rand-cell)
+           (link cell rand-cell)))))
+   grid))
