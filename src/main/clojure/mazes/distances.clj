f6468562325076515b5c967f6a9d59d5e6b03391
Guillermo Szeliga <gszeliga@gmail.com>
2017-04-07 17:45:06 +0200

    Fixup distances implementation

--- src/main/clojure/mazes/distances.clj
+++ src/main/clojure/mazes/distances.clj
@@ -1,21 +1,22 @@
 (ns mazes.distances
-  (require [mazes.cell :refer :all]))
+  (require [mazes.cell :refer :all])
+  (use [mazes.grid :only (cell-at)]))
 
-(defn distances-from [cell]
+(defn distances-from [cell grid]
 
-  (defn next-pass [frontiers distances]
-    (reduce (fn [cell state]
-              (let [[new-frontiers distances] state]
-                (doseq [linked (links cell)]
-                  (if-not (contains? distances (to-id linked))
-                    [(conj linked new-frontiers) (assoc distances (to-id linked) (inc ((to-id cell) distances)))]
-                    state))))
-            [(empty frontiers) distances] frontiers))
+  (defn next-pass [frontiers distances step]
+    (reduce (fn [state linked]
+              (let [[new-frontiers new-distances] state]
+                (if-not (contains? new-distances linked)
+                  [(conj new-frontiers linked) (assoc new-distances linked (inc step))]
+                  state)))
+            [(empty frontiers) distances]
+            (mapcat identity (map #(links (apply cell-at grid %)) frontiers))))
 
-  (defn get-distances [frontiers distances]
+  (defn get-distances [frontiers distances step]
     (if-not (empty? frontiers)
-      (let [[f d] (next-pass frontiers distances)]
-        (recur f d))
+      (let [[f d] (next-pass frontiers distances step)]
+        (recur f d (inc step)))
       distances))
 
-  (get-distances [cell] {}))
+  (get-distances [(to-id cell)] {(to-id cell) 0} 0))
