0165d5e70ae7318b4cab155ee075d42b9c2e74d4
Guillermo Szeliga <gszeliga@gmail.com>
2017-04-11 21:58:06 +0200

    Minor enhancements over distances

--- src/main/clojure/mazes/distances.clj
+++ src/main/clojure/mazes/distances.clj
@@ -2,21 +2,25 @@
   (require [mazes.cell :refer :all])
   (use [mazes.grid :only (cell-at)]))
 
-(defn distances-from [cell grid]
+(defn distances-from
+  ([row col grid]
+   (distances-from (cell-at grid row col) grid))
 
-  (defn next-pass [frontiers distances step]
-    (reduce (fn [state linked]
-              (let [[new-frontiers new-distances] state]
-                (if-not (contains? new-distances linked)
-                  [(conj new-frontiers linked) (assoc new-distances linked (inc step))]
-                  state)))
-            [(empty frontiers) distances]
-            (mapcat identity (map #(links (apply cell-at grid %)) frontiers))))
+  ([cell grid]
+   (defn next-pass [frontiers distances step]
+     (reduce (fn [state linked]
+               (let [[next-cells partial-distances] state]
+                 (if-not (contains? partial-distances linked)
+                   [(conj next-cells linked) (assoc partial-distances linked (inc step))]
+                   state)))
+             [(empty frontiers) distances]
+             (mapcat identity (map #(links (apply cell-at grid %)) frontiers))))
 
-  (defn get-distances [frontiers distances step]
-    (if-not (empty? frontiers)
-      (let [[f d] (next-pass frontiers distances step)]
-        (recur f d (inc step)))
-      distances))
+   (defn get-distances [frontiers distances step]
+     (if-not (empty? frontiers)
+       (let [[f d] (next-pass frontiers distances step)]
+         (recur f d (inc step)))
+       distances))
 
-  (get-distances [(to-id cell)] {(to-id cell) 0} 0))
+   (let [cell-id (to-id cell)]
+     (get-distances [cell-id] {cell-id 0} 0))))
