c5fad64e0f020ca7a22f49a6e4c1046271bf151d
Guillermo Szeliga <gszeliga@gmail.com>
2017-08-29 22:39:03 +0200

    Implement reading a Mask definition from 'txt' files

--- src/main/clojure/mazes/mask.clj
+++ src/main/clojure/mazes/mask.clj
@@ -16,7 +16,8 @@
   (size [self]
     (.cardinality (. self bits)))
   (define [self row col v]
-    (.set (. self bits) (+ (* row (. self cols)) col) v)))
+    (.set (. self bits) (+ (* row (. self cols)) col) v)
+    self))
 
 (defn make-mask [rows columns]
   (let [size (* rows columns)
@@ -26,7 +27,14 @@
 
 (defn from-txt [file]
   (with-open [rdr (io/reader file)]
-    (doseq [line (->> (reverse (line-seq rdr))
-                      (map-indexed (fn [row-idx lchars]
-                                     (map-indexed #(vector [row-idx %1] %2) lchars))))]
-      (print line))))
+    (let [bits-idx (into () ;it sucks that it can't be lazy but I need [rows,columns]
+                         (for [[line-idx,line] (map-indexed #(vector %1 %2) (reverse (line-seq rdr)))
+                               cols-idx (map-indexed #(vector [line-idx %1] (== (int \.) (int %2))) line)]
+                           cols-idx))
+          ;little 'trick' here: since a list guarantees 'head' to be the latest inserted element, it is expected to
+          ;contain the upper-right most cell coordinate 'matching' the number of rows & cols in the grid
+          [rows,cols] (->> bits-idx first first (map inc))]
+
+      (reduce (fn [mask [[row col] bit]]
+                (define mask row col bit))
+              (make-mask rows cols) bits-idx))))
