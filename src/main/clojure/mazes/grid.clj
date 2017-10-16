d16b29f7af98c8bed95d360362e694bcc8b428dc
Guillermo Szeliga <gszeliga@gmail.com>
2017-10-17 00:39:44 +0200

    Clean up code a little bit

--- src/main/clojure/mazes/grid.clj
+++ src/main/clojure/mazes/grid.clj
@@ -57,24 +57,18 @@
 (defmulti cell-at 
   (fn [grid & _] (-> grid meta :type)))
 
-(defmethod cell-at :standard
-  ([grid cell]
-   (cell-at grid (:row cell) (:column cell)))
-  ([grid row col]
-   (get-in grid [row col])))
-
-(defmethod cell-at :hex
-  ([grid cell]
-   (cell-at grid (:row cell) (:column cell)))
-  ([grid row col]
-   (get-in grid [row col])))
-
 (defmethod cell-at :polar
   ([grid cell]
    (cell-at grid (:row cell) (:column cell)))
   ([grid row col]
    (let [adj-col (mod col (-> grid (get row) count))]
      (get-in grid [row adj-col]))))
+
+(defmethod cell-at :default
+  ([grid cell]
+   (cell-at grid (:row cell) (:column cell)))
+  ([grid row col]
+   (get-in grid [row col])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                    Grid definition
@@ -102,7 +96,6 @@
 (defmethod make-grid :standard
   ([t rows columns]
    (make-grid t rows columns (make-mask rows columns)))
-
   ([t rows columns mask]
    (with-meta (into [] (map
                         (fn [r]
@@ -146,27 +139,31 @@
 ;                       Neighbors resolution
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn- ^:private neighbors-fn 
+  [row col grid coords-fn]
+  (reduce-kv
+   (fn [m k coord]
+     (let [cell    (when coord (apply cell-at grid coord))
+           is-dead (if (some? cell) (dead? cell) true)]
+       (assoc m k (when-not is-dead cell))))
+   {}
+   (coords-fn row col)))
+
 (defmulti ^:private neighbors-from
   (fn [row col grid] (-> grid meta :type)))
 
 (defmethod neighbors-from :standard
   [row col grid]
-  (letfn [(neighbors-at [row column]
+  (letfn [(coords-fn [row column]
             {:north [(inc row) column]
              :south [(dec row) column]
              :west  [row (dec column)]
              :east  [row (inc column)]})]
-    (reduce-kv
-     (fn [m k coord]
-       (let [cell    (apply cell-at grid coord)
-             is-dead (if (some? cell) (dead? cell) true)]
-         (assoc m k (when-not is-dead cell))))
-     {}
-     (neighbors-at row col))))
+    (neighbors-fn row col grid coords-fn)))
 
 (defmethod neighbors-from :hex
   [row col grid]
-  (letfn [(neighbors-at [row col]
+  (letfn [(coords-fn [row col]
             (let [north-diagonal (if (even? col) (dec row) row)
                   south-diagonal (if (even? col) row (inc row))]
               {:northwest [north-diagonal (dec col)]
@@ -175,39 +172,28 @@
                :southwest [south-diagonal (dec col)]
                :south     [(inc row) col]
                :southeast [south-diagonal (inc col)]}))]
-    (reduce-kv
-     (fn [m k coord]
-       (let [cell    (apply cell-at grid coord)
-             is-dead (if (some? cell) (dead? cell) true)]
-         (assoc m k (when-not is-dead cell))))
-     {}
-     (neighbors-at row col))))
+    (neighbors-fn row col coords-fn)))
 
 (defmethod neighbors-from :polar
   [row col grid]
-  (letfn [(ratio [row grid]
+  (letfn [(ratio-fn [row grid]
             (when (and (-> row zero? not)
                        (< row (n-rows grid)))
               (let [c-row-length (-> grid (get row) count)
                     p-row-length (-> grid (get (dec row)) count)]
                 (/ c-row-length p-row-length))))
 
-          (neighbors-at [row column]
-                        (if (zero? row)
-                          {:cw nil :ccw nil :outward nil :inward nil}
-                          (let [o-ratio (ratio (inc row) grid)
-                                c-ratio (ratio row grid)]
-                            {:cw      [row (inc column)]
-                             :ccw     [row (dec column)]
-                             :outward (when o-ratio [(inc row) (* column o-ratio)])
-                             :inward  [(dec row) (/ column c-ratio)]})))]
+          (coords-fn [row column]
+            (if (zero? row)
+              {:cw nil :ccw nil :outward nil :inward nil}
+              (let [o-ratio (ratio-fn (inc row) grid)
+                    c-ratio (ratio-fn row grid)]
+                {:cw      [row (inc column)]
+                 :ccw     [row (dec column)]
+                 :outward (when o-ratio [(inc row) (* column o-ratio)])
+                 :inward  [(dec row) (/ column c-ratio)]})))]
 
-    (reduce-kv
-     (fn [m k coord]
-       (let [cell (when coord (apply cell-at grid coord))]
-         (assoc m k cell)))
-     {}
-     (neighbors-at row col))))
+    (neighbors-fn row col grid coords-fn)))
 
 (defn neighbors
   ([cell grid]
@@ -217,4 +203,3 @@
   ([row col grid state]
    (into {} (comp (remove :dead)
                   (state cell-xforms)) (neighbors-from row col grid))))
-
