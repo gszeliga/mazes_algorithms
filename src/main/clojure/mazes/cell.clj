6dcd145dd93a4733a1e22ff88baaee41e67d8480
Guillermo Szeliga <gszeliga@gmail.com>
2017-12-17 18:42:30 +0100

    Defining Weaved cells

--- src/main/clojure/mazes/cell.clj
+++ src/main/clojure/mazes/cell.clj
@@ -1,7 +1,5 @@
 (ns mazes.cell)
 
-(defrecord Cell [row column links dead])
-
 (defprotocol Linkable
   (to-id [self])
   (linked? [self cell])
@@ -14,36 +12,54 @@
     [self cell bidi])
   (links [self]))
 
-(extend-protocol Linkable
-  Cell
+(defprotocol Weaved
+  (over?  [self])
+  (under? [self])
+  (vert-pos [self pos])
+  (horizontal-passage? [self neighbors])
+  (vertical-passage? [self neighbors]))
+
+(defrecord Cell [row column links dead]
+  Linkable
+
   (to-id [self]
     [(:row self) (:column self)])
 
-  (dead? [self]
-    (:dead self))
+  (dead? [self] (:dead self))
 
   (linked? [self cell]
     (if cell
-      (contains? (-> self :links deref) (to-id cell))
+      (contains? (-> self :links deref)
+                 (to-id cell))
       false))
 
-  (link
-    ([self cell] (link self cell true))
-    ([self cell bidi]
-     (dosync
-      (alter (:links self) assoc (to-id cell) cell))
-     (when bidi (link cell self false))))
+  (link [self cell]
+    (link self cell true))
 
-  (unlink
-    ([self cell] (unlink cell true))
-    ([self cell bidi]
-     (dosync
-      (alter (:links self) dissoc (to-id cell)))
-     (when bidi (unlink cell self false))))
+  (link [self cell bidi]
+    (dosync
+     (alter (:links self) assoc (to-id cell) cell))
+    (when bidi (link cell self false)))
+
+  (unlink [self cell]
+    (unlink cell true))
+
+  (unlink [self cell bidi]
+    (dosync
+     (alter (:links self) dissoc (to-id cell)))
+    (when bidi (unlink cell self false)))
 
   (links [self] (keys (-> self :links deref))))
 
+(defrecord WeavedCell [row column links dead vert-pos]
+  Weaved
+  (over? [self]
+    (-> self :vert-pos deref (= :over)))
+
+  (under? [self]
+    (-> self :vert-pos deref (= :under))))
+
 (defn make-cell
-  [& {:keys [row column links dead]
-      :or {dead false links {}}}]
-  (Cell. row column (ref links) dead))
+      [& {:keys [row column links dead]
+          :or {dead false links {}}}]
+      (Cell. row column (ref links) dead))
