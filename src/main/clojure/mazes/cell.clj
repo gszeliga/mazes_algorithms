f63ff891f27ebffb3981e120585a8dd4da11baa6
Guillermo Szeliga <gszeliga@gmail.com>
2017-12-27 19:39:41 +0100

    After some analysis, initial layout done

--- src/main/clojure/mazes/cell.clj
+++ src/main/clojure/mazes/cell.clj
@@ -1,5 +1,10 @@
 (ns mazes.cell)
 
+(defn ^:private link-fn [from-cell to-cell bidi]
+  (dosync
+   (alter (:links from-cell) assoc (to-id to-cell) to-cell))
+  (when bidi (link-fn to-cell from-cell false)))
+
 (defprotocol Linkable
   (to-id [self])
   (linked? [self cell])
@@ -13,51 +18,42 @@
   (links [self]))
 
 (defprotocol Weaved
-  (over?  [self])
-  (under? [self])
-  (vert-pos [self pos])
-  (horizontal-passage? [self neighbors])
-  (vertical-passage? [self neighbors]))
+  (tunnel! [self cell])
+  (tunnels [self]))
 
 (defrecord Cell [row column links dead]
   Linkable
-
   (to-id [self]
     [(:row self) (:column self)])
-
   (dead? [self] (:dead self))
-
   (linked? [self cell]
     (if cell
       (contains? (-> self :links deref)
                  (to-id cell))
       false))
-
   (link [self cell]
     (link self cell true))
-
   (link [self cell bidi]
-    (dosync
-     (alter (:links self) assoc (to-id cell) cell))
-    (when bidi (link cell self false)))
-
+    (link-fn self cell bidi))
   (unlink [self cell]
     (unlink cell true))
-
   (unlink [self cell bidi]
     (dosync
      (alter (:links self) dissoc (to-id cell)))
     (when bidi (unlink cell self false)))
-
   (links [self] (keys (-> self :links deref))))
 
-(defrecord WeavedCell [row column links dead vert-pos]
+(defrecord WeavedCell [row column links dead tunnels tunnel-fn]
   Weaved
-  (over? [self]
-    (-> self :vert-pos deref (= :over)))
-
-  (under? [self]
-    (-> self :vert-pos deref (= :under))))
+  (tunnel! [self cell]
+    (swap! (self :tunnels) assoc (to-id cell) cell))
+  (tunnels [self]
+    (-> self :tunnels deref keys))
+  
+  Linkable
+  (link [self cell bidi]
+    (when (not (tunnel-fn self cell)) 
+      (link-fn self cell bidi))))
 
 (defn make-cell
       [& {:keys [row column links dead]
